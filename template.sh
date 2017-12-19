42bedf0245dda643d62a1ff324ca229d3218ae79
James Puellmann <jpuellma@users.noreply.github.com>
2017-12-19 12:38:54 -0500

    Update template.sh

--- template.sh
+++ template.sh
@@ -25,12 +25,15 @@ log_info() {
 
 do_get_args() {
     log_debug "Begin ${FUNCNAME[0]}()."
+    local ret_val
     log_debug "End ${FUNCNAME[0]}()."
+    return ${ret_val}
 }
 
 
 do_print_usage() {
     log_debug "Begin ${FUNCNAME[0]}."
+    local ret_val
     cat << eof
 Usage: ${__name} [ option1| option2 | option3 ]
     option1 - Does thing1
@@ -38,31 +41,40 @@ Usage: ${__name} [ option1| option2 | option3 ]
     option3 - Does thing3
 eof
     log_debug "End ${FUNCNAME[0]}."
+    return ${ret_val}
 }
 
 
 do_stop() {
     log_debug "Begin ${FUNCNAME[0]}()."
+    local ret_val
     log_debug "End ${FUNCNAME[0]}()."
+    return ${ret_val}
 }
 
 
 do_start() {
     log_debug "Begin ${FUNCNAME[0]}()."
+    local ret_val
     log_debug "End ${FUNCNAME[0]}()."
+    return ${ret_val}
 }
 
 
 do_restart(){
     log_debug "Begin ${FUNCNAME[0]}()."
+    local ret_val
     do_stop
+    let ret_val=ret_val+$?
     do_start
     log_debug "End ${FUNCNAME[0]}()."
+    return ${ret_val}
 }
 
 
 main() {
     log_debug "Begin ${FUNCNAME[0]}()."
+    return_value=0
     __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
     __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
     __name="$(basename ${__file})"
@@ -71,6 +83,7 @@ main() {
     log_debug "__name is ${__name}."
     # DO WORK HERE
     log_debug "End ${FUNCNAME[0]}()."
+    return ${return_value}
 }
 
 
